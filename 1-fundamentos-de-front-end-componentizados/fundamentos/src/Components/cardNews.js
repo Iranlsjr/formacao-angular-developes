01d407dca5333185ebfe3a9b671f69f2aead09fd
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-06 21:25:25 -0300

    Setando Propriedades de classes

--- 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
+++ 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
@@ -1,9 +1,33 @@
-class CardNews extends HTMLElement {
+class Cardnews extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({mode: "open"})
-    shadow.innerHTML = "<h1>hello world</h1>"
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+    //shadow.innerHTML = "<h1>Construindo um Component de Maneira profissional</h1>";
   }
+
+  //metodo
+  build() {
+    const ComponentRoot = document.createElement("div");
+    ComponentRoot.setAttribute("class", "card")
+
+    const card__left = document.createElement("div");
+    card__left.setAttribute("class", "card__left")
+
+
+    const card__rigth = document.createElement("div");
+    card__rigth.setAttribute("class", "card__rigth")
+
+
+    ComponentRoot.appendChild(card__left);
+    ComponentRoot.appendChild(card__rigth);
+
+    return ComponentRoot;
+  }
+  //metodo
+  styles() {}
 }
 
-customElements.define('card-news', CardNews)
+customElements.define("card-news", Cardnews);
