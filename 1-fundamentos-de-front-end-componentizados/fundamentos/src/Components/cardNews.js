86452b2286df13c11006de33eebce33f6009acd7
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-06 22:31:04 -0300

    Construindo o Build do Componet

--- 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
+++ 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
@@ -11,15 +11,25 @@ class Cardnews extends HTMLElement {
   //metodo
   build() {
     const ComponentRoot = document.createElement("div");
-    ComponentRoot.setAttribute("class", "card")
+    ComponentRoot.setAttribute("class", "card");
+//---------------------------------------------------------------------------------------------------
 
     const card__left = document.createElement("div");
-    card__left.setAttribute("class", "card__left")
+    card__left.setAttribute("class", "card__left");
 
-
+    const autor = document.createElement("span");
+    const linkTitle = document.createElement("a");
+    const newsContent = document.createElement("p");
+    card__left.appendChild(autor)
+    card__left.appendChild(linkTitle)
+    card__left.appendChild(newsContent)
+//---------------------------------------------------------------------------------------------------
     const card__rigth = document.createElement("div");
-    card__rigth.setAttribute("class", "card__rigth")
-
+    card__rigth.setAttribute("class", "card__rigth");
+    
+    const newsImage = document.createElement("img")
+    card__rigth.appendChild(newsImage)
+//---------------------------------------------------------------------------------------------------
 
     ComponentRoot.appendChild(card__left);
     ComponentRoot.appendChild(card__rigth);
