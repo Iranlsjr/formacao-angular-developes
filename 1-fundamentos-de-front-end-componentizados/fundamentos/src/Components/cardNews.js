461de7fded818bfd4ea53f9c81062fa4a0854910
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-07 08:55:58 -0300

    Criando o Style do Component

--- 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
+++ 1-fundamentos-de-front-end-componentizados/fundamentos/src/Components/cardNews.js
@@ -12,7 +12,7 @@ class Cardnews extends HTMLElement {
   build() {
     const ComponentRoot = document.createElement("div");
     ComponentRoot.setAttribute("class", "card");
-//---------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------
 
     const card__left = document.createElement("div");
     card__left.setAttribute("class", "card__left");
@@ -21,24 +21,25 @@ class Cardnews extends HTMLElement {
     autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
 
     const linkTitle = document.createElement("a");
-    linkTitle.textContent = this.getAttribute("title")
-    linkTitle.href = this.getAttribute("link-url")
+    linkTitle.textContent = this.getAttribute("title");
+    linkTitle.href = this.getAttribute("link-url");
 
     const newsContent = document.createElement("p");
-    newsContent.textContent = this.getAttribute("content")
+    newsContent.textContent = this.getAttribute("content");
 
-    card__left.appendChild(autor)
-    card__left.appendChild(linkTitle)
-    card__left.appendChild(newsContent)
-//---------------------------------------------------------------------------------------------------
+    card__left.appendChild(autor);
+    card__left.appendChild(linkTitle);
+    card__left.appendChild(newsContent);
+    //---------------------------------------------------------------------------------------------------
     const card__rigth = document.createElement("div");
     card__rigth.setAttribute("class", "card__rigth");
-    
-    const newsImage = document.createElement("img")
-    newsImage.src = this.getAttribute("photo")  || "assets/img/foto-default.avif"
-    newsImage.alt = "Foto da Notícia"
-    card__rigth.appendChild(newsImage)
-//---------------------------------------------------------------------------------------------------
+
+    const newsImage = document.createElement("img");
+    newsImage.src =
+      this.getAttribute("photo") || "assets/img/foto-default.avif";
+    newsImage.alt = "Foto da Notícia";
+    card__rigth.appendChild(newsImage);
+    //---------------------------------------------------------------------------------------------------
 
     ComponentRoot.appendChild(card__left);
     ComponentRoot.appendChild(card__rigth);
@@ -46,7 +47,51 @@ class Cardnews extends HTMLElement {
     return ComponentRoot;
   }
   //metodo
-  styles() {}
+  styles() {
+    const style = document.createElement("style");
+    style.textContent = `
+    :root {
+  --color: #343232;
+}
+* {
+  font-family: "Segoe UI", sans-serif;
+}
+.card {
+  width: 100%;
+  display: flex;
+  box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+  justify-content: space-between;
+}
+.card__left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 10px;
+}
+.card__left > span {
+  font-weight: 400;
+}
+.card__left > a {
+  margin-top: 15px;
+  font-size: 25px;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+.card__left > p {
+  color: var(--color);
+}
+.card__rigth > img {
+  width: 250px;
+}
+
+    
+    `;
+
+    return style;
+  }
 }
 
 customElements.define("card-news", Cardnews);
