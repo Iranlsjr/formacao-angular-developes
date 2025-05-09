301dc185809495633709e29b523131f1ca621f15
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-05-09 11:46:32 -0300

    Trabalhando com tipo de variáveis

--- 1-fundamentos-de-front-end-componentizados/typescript/build/index.js
+++ 1-fundamentos-de-front-end-componentizados/typescript/build/index.js
@@ -1,8 +1,25 @@
 "use strict";
-function printaObjeto(pessoa) {
+function printaObjeto(gitgipessoa) {
     console.log(pessoa);
 }
 printaObjeto({
     name: "Bruce Wayne",
     vulgo: "Batman",
 });
+//Variáveis
+//Tipos Primitivos: boolean
+//Tipos Primitivos: number
+//Tipos Primitivos: String
+let ligado = false;
+let nome = "Iran";
+let idade = 30;
+let altura = 1.72;
+//Tipos especiais: null
+//Tipos especiais: undefined
+let nulo = null;
+let indefinido = undefined;
+// Tipo Abrangentes: any
+// Tipo Abrangentes: void
+let retorno;
+function executQuery() { }
+let retornoView = "Aceita qualquer coisa";
--- 1-fundamentos-de-front-end-componentizados/typescript/src/index.ts
+++ 1-fundamentos-de-front-end-componentizados/typescript/src/index.ts
@@ -3,11 +3,31 @@ type heroi = {
   vulgo: string;
 };
 
-function printaObjeto(gitgipessoa: heroi){
-console.log(pessoa)
-
+function printaObjeto(gitgipessoa: heroi) {
+  console.log(pessoa);
 }
 printaObjeto({
   name: "Bruce Wayne",
   vulgo: "Batman",
-})
+});
+
+//Variáveis
+
+//Tipos Primitivos: boolean
+//Tipos Primitivos: number
+//Tipos Primitivos: String
+let ligado: boolean = false;
+let nome: string = "Iran";
+let idade: number = 30;
+let altura: number = 1.72;
+
+//Tipos especiais: null
+//Tipos especiais: undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+// Tipo Abrangentes: any
+// Tipo Abrangentes: void
+let retorno: void 
+function executQuery(): void{}
+let retornoView: any =  "Aceita qualquer coisa"
