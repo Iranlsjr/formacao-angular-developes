6b22ee097b030d98aba4199bd32e4c7842d5fa3d
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-06-11 17:31:13 -0300

    creating typescript

---
+++ 01-fundamentos-de-front-end-componentizados/02-typescript/src/index.ts
@@ -0,0 +1,12 @@
+type heroi = {
+  name: string;
+  vulgo: string;
+};
+
+function printaObjeto(pessoa: heroi) {
+  console.log(pessoa);
+}
+printaObjeto({
+  name: "Bruce wayne",
+  vulgo: "Batman",
+});
